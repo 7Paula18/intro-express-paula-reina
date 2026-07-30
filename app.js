66b709176e2e248d1a846767cd3d2c4b514653e2
joretopa7-stack <joretopa7@gmail.com>
2026-07-30 13:48:52 -0500

    agregue una importación y crear endpoints

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 import express from 'express'
 import "dotenv/config"
-
+//const bodyParser = require('body-parser');//importacion commonjs
+import bodyParser from "body-parser";//importacion ES "module"
 const app = express(); 
 const port = process.env.port || 3000; 
+
+//configurar el uso de body-parser para la aplicación - no se esta usando
+app.use(express)
+
+app.get("/")
+
+//otro endpoint, funcion de ficha
+app.get("/productos", (res, res)=>{
+  //usando template string ""
+})
 
 app.get("/", (_, res) => { 
   res.send('Hola, estamos aprendiendo express con la ficha 3407184'); 
